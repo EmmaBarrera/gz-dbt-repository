7c183dc837d2a0e402346826836253002e1c5b99
Emmanuel Barrera <emmanuelbarrera.coach@gmail.com>
2024-08-13 22:58:18 +0000

    finance_day mart finished

--- models/staging/mart/finance_days.sql
+++ models/staging/mart/finance_days.sql
@@ -1,7 +1,13 @@
-WITH daily_data AS (
-    SELECT
-        date_date
-        ,COUNT(orders_id) AS nb_transactions
-        ,SUM(revenue) AS revenue
-        ,AVG(quantity) AS average_basket
-)
+SELECT
+    date_date
+    ,COUNT(DISTINCT orders_id) AS nb_transactions
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(AVG(revenue),2) AS average_basket
+    ,ROUND(SUM(margin),2) AS margin
+    ,ROUND(SUM(operational_margin),2) AS operational_margin
+FROM
+    {{ref("int_orders_operational")}}
+GROUP BY
+    date_date
+ORDER BY
+    date_date DESC
